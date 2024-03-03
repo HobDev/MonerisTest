6f4c2533c2bcb528194845c6b147a49486bdbeb8
Paramjit Singh <sparamjit92@gmail.com>
2024-03-03 21:27:09 +0530

    add few comments about the type of transactions

--- MonerisTest/AppShell.cs
+++ MonerisTest/AppShell.cs
@@ -7,15 +7,15 @@ namespace MonerisTest
         {
             Routing.RegisterRoute(nameof(PaymentWebPage), typeof(PaymentWebPage));
 
-          
-            // pay for the booking
+
+            // pay for the booking by using Hosted tokenization. If the permanent token is saved use it to make a vault payment. If the permanent token is not saved, then first create a temporary token, and if customer allows convert it to a permanent token and then make a payment using the permanent token. If customer don't allow to save the permanent token, then make a payment using the temporary token.
             Items.Add(new ShellContent
             {
                 Title = "booking",
                 ContentTemplate = new DataTemplate(typeof(PaymentPage))
             });
 
-            // cancel the booking   
+            // first initiate the refund and then cancel the booking. The refund is done if the payment is already settled by the payment gateway. Both the payment and the refund will be shown in the bank statement of the customer.
             Items.Add(new ShellContent
             {
                 Title = "Cancellation",
@@ -36,8 +36,7 @@ namespace MonerisTest
                 ContentTemplate = new DataTemplate(typeof(PaymentWebPage))
             });
 
-            // charge convenience fee   
-
+            // charge convenience fee on every purchase. The convenience fee is charged by the merchant for providing the convenience of making the payment online. The convenience fee is charged by the merchant for providing the convenience of booking online. The purpose of the convenience fee is to cover the cost of the payment gateway and the cost of the convenience provided to the customer. The convenience fee is charged on the total amount of the booking.The convenience fee and the booking amount will be shown separately in the bank statement of the customer.
             Items.Add(new ShellContent
             {
                 Title = "Convenience Fee",
